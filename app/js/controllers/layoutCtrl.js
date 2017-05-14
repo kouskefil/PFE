a06417b236551afe85edb2cc29ee9aca955232d3
Kefil KOUSSONDA <kk@takatel.net>
2017-05-14 11:43:14 +0100

    realisation de new user en popup

--- app/js/controllers/layoutCtrl.js
+++ app/js/controllers/layoutCtrl.js
@@ -21,8 +21,8 @@ define(['angular'], function (angular) {
                 item.action();
             };
              $scope.go = function () {
-                // $state.go('root.install',{module:'admin'});
-                 $state.go('root.app');
+                $state.go('root.install',{module:'admin'});
+                 // $state.go('root.app');
              };
             /*Layout vars config functions*/
             $scope.navbarCollapsed = true;
